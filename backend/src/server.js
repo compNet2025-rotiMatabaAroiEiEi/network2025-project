9e98f0404bb16c56e226762a62bc1ae0ae3573ec
nattharat-naja <nattharatkl@gmail.com>
2025-11-10 21:56:36 +0700

    🎉🐳start the project with docker

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+
+//  before release update this to config.env file
+dotenv.config({ path: path.join(__dirname, "config", "config.env.testing") });
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("DB URL at runtime:", process.env.DATABASE_URL);
+});
